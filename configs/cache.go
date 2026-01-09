25e9f8d64ac144d83eae60cff9de4687548518d3
Adrian Kurek <adikurek11@gmail.com>
2026-01-09 19:10:34 +0100

    feat(config/helpers):Created config for cache and db and helpers Created helpers for request and response operations

---
+++ configs/cache.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type CacheService struct {
+	client *redis.Client
+}
+
+func NewCacheService(cacheLink string) (*CacheService, error) {
+	opt, err := redis.ParseURL(cacheLink)
+	if err != nil {
+		return &CacheService{}, err
+	}
+
+	return &CacheService{
+		client: redis.NewClient(opt),
+	}, nil
+}
+
+func (c *CacheService) SetData(ctx context.Context, key string, data string, ttl time.Duration) error {
+	err := c.client.Set(ctx, key, data, ttl).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *CacheService) GetData(ctx context.Context, key string) (string, error) {
+	res, err := c.client.Get(ctx, key).Result()
+	if err != nil {
+		return "", err
+	}
+
+	return res, nil
+}
+
+func (c *CacheService) ExistsData(ctx context.Context, key string) (int64, error) {
+	res, err := c.client.Exists(ctx, key).Result()
+	if err != nil {
+		return 0, err
+	}
+
+	return res, nil
+}
+
+func (c *CacheService) DeleteData(ctx context.Context, key string) error {
+	err := c.client.Del(ctx, key).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
