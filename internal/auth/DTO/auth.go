24d6229b84be6e3db355c5ba2f8288ae630efa27
slodkiadrianek <adikurek11@gmail.com>
2026-01-28 13:00:43 +0100

    feat(validator):introduced validation tags

--- internal/auth/DTO/auth.go
+++ internal/auth/DTO/auth.go
@@ -1,8 +1,8 @@
 package dto
 
 type CreateUser struct {
-	Username        string
-	Email           string
-	Password        string
-	ConfirmPassword string
+	Username        string `validate:"required,min=6"`
+	Email           string `validate:"required,email"`
+	Password        string `validate:"required,min=12,strong_password"`
+	ConfirmPassword string `validate:"required,eqfield=Password"`
 }
