f640aea7f32768a76618978d336dc2deab871952
Adrian Kurek <adikurek11@gmail.com>
2026-06-04 16:40:43 +0200

    feat: completed service and controller for create bucket

--- internal/bucket/controller/bucket.go
+++ internal/bucket/controller/bucket.go
@@ -1,12 +1,22 @@
 package controller
 
 import (
+	"context"
+	"errors"
 	"net/http"
+	"time"
 
+	bucketDTO "github.com/slodkiadrianek/MINI-BUCKET/internal/bucket/DTO"
+	commonErrors "github.com/slodkiadrianek/MINI-BUCKET/internal/common/errors"
 	commonInterfaces "github.com/slodkiadrianek/MINI-BUCKET/internal/common/interfaces"
+	"github.com/slodkiadrianek/MINI-BUCKET/internal/common/request"
+	"github.com/slodkiadrianek/MINI-BUCKET/internal/common/response"
+	"github.com/slodkiadrianek/MINI-BUCKET/internal/middleware"
 )
 
-type bucketService interface{}
+type bucketService interface {
+	Create(ctx context.Context, userID int, bucket bucketDTO.CreateBucket) error
+}
 
 type BucketController struct {
 	bucketService bucketService
@@ -23,5 +33,32 @@ func NewBucketController(bucketService bucketService, authorization commonInterf
 }
 
 func (bc *BucketController) CreateBucket(w http.ResponseWriter, r *http.Request) error {
-}
+	ctx, cancel := context.WithTimeout(r.Context(), time.Second*2)
+	defer cancel()
 
+	reqData, err := request.ReadBody[bucketDTO.CreateBucket](r)
+	if err != nil {
+		return commonErrors.NewAPIError(http.StatusUnprocessableEntity, "provided invalid json format")
+	}
+
+	err = middleware.ValidateRequestData(reqData)
+	if err != nil {
+		return err
+	}
+
+	userID, err := request.ReadUserIDFromToken(r)
+	if err != nil {
+		return err
+	}
+
+	err = bc.bucketService.Create(ctx, userID, *reqData)
+	if err != nil {
+		if errors.Is(err, context.DeadlineExceeded) {
+			bc.loggerService.Info("request timed out", r.URL.Path)
+			return commonErrors.NewAPIError(http.StatusRequestTimeout, "")
+		}
+		return err
+	}
+	response.Send(w, 201, nil)
+	return nil
+}
--- internal/bucket/service/bucket.go
+++ internal/bucket/service/bucket.go
@@ -1,13 +1,21 @@
 package service
 
 import (
+	"context"
+
+	bucketDTO "github.com/slodkiadrianek/MINI-BUCKET/internal/bucket/DTO"
 	commonInterfaces "github.com/slodkiadrianek/MINI-BUCKET/internal/common/interfaces"
 )
 
-type bucketRepository interface{}
+type bucketRepository interface {
+	CreateBucket(ctx context.Context, userID int, bucket bucketDTO.CreateBucket) error
+}
 
 type BucketService struct {
 	bucketRepository bucketRepository
 	logger           commonInterfaces.Logger
 }
 
+func (bs *BucketService) Create(ctx context.Context, userID int, bucket bucketDTO.CreateBucket) error {
+	return bs.bucketRepository.CreateBucket(ctx, userID, bucket)
+}
