10260219b491ad6aeb15160a7c12040dd21eae79
slodkiadrianek <adikurek11@gmail.com>
2026-06-11 16:52:56 +0200

    feat: added json fields to DTO's

--- internal/auth/DTO/auth.go
+++ internal/auth/DTO/auth.go
@@ -1,13 +1,13 @@
 package dto
 
 type CreateUser struct {
-	Username        string `validate:"required,min=6"`
-	Email           string `validate:"required,email"`
-	Password        string `validate:"required,min=12"`
-	ConfirmPassword string `validate:"required,eqfield=Password"`
+	Username        string `json:"username" validate:"required,min=6"`
+	Email           string `json:"email" validate:"required,email"`
+	Password        string `json:"password" validate:"required,min=12"`
+	ConfirmPassword string `json:"confirmPassword" validate:"required,eqfield=Password"`
 }
 
 type LoginUser struct {
-	Email    string `validate:"required,email"`
-	Password string `validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
 }
