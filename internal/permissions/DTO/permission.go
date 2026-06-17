3c52de86761c68093328914f73e017b2504d91d8
Adrian Kurek <adikurek11@gmail.com>
2026-06-17 17:00:44 +0200

    tests: created tests scenarios for create method of permission handler

--- internal/permissions/DTO/permission.go
+++ internal/permissions/DTO/permission.go
@@ -1,8 +1,8 @@
 package dto
 
 type Upsert struct {
-	UserID     int `json:"userID" validate:"required, number"`
-	Permission int `json:"permission" validate:"required,number ,oneof=1 2 4 3 5 6 7"`
+	UserID     int `json:"userID" validate:"required,number"`
+	Permission int `json:"permission" validate:"required,number,oneof=1 2 4 3 5 6 7"`
 }
 
 type Delete struct {
