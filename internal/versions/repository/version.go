dd2bbb9659e30c7e1ba899fd993422ec71012409
Adrian Kurek <adikurek11@gmail.com>
2026-06-21 15:18:23 +0200

    feat: created version package and moved version db function to version repository

---
+++ internal/versions/repository/version.go
@@ -0,0 +1,143 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	commonErrors "github.com/slodkiadrianek/MINI-BUCKET/common/errors"
+	commonInterfaces "github.com/slodkiadrianek/MINI-BUCKET/common/interfaces"
+	dto "github.com/slodkiadrianek/MINI-BUCKET/internal/objects/DTO"
+)
+
+type VersionRepository struct {
+	db            *sql.DB
+	loggerService commonInterfaces.Logger
+}
+
+func NewVersionRepository(db *sql.DB, loggerService commonInterfaces.Logger) *VersionRepository {
+	return &VersionRepository{
+		db:            db,
+		loggerService: loggerService,
+	}
+}
+
+func (ov *VersionRepository) CreateVersion(ctx context.Context, tx *sql.Tx, file dto.CreateVersion) (int, error) {
+	query := `INSERT INTO object_versions (
+		object_id,
+		version_number,
+		size_bytes,
+		etag,
+		storage_class,
+		created_at,
+		updated_at
+	) VALUES($1,$2,$3,$4,$5,NOW(), NOW()) RETURNING id`
+
+	stmt, err := tx.PrepareContext(ctx, query)
+	if err != nil {
+		ov.loggerService.Error(commonErrors.FailedToPrepareQuery, map[string]any{
+			"query": query,
+			"args": map[string]any{
+				"object_id":      file.ObjectID,
+				"version_number": file.VersionNumber,
+				"size_bytes":     file.SizeBytes,
+				"etag":           file.ETag,
+				"storage_class":  file.StorageClass,
+			},
+			"error": err.Error(),
+		})
+		return 0, err
+	}
+	defer func() {
+		if closeErr := stmt.Close(); closeErr != nil {
+			ov.loggerService.Error(commonErrors.FailedToCloseStatement, closeErr)
+		}
+	}()
+
+	var newVersionID int
+	err = stmt.QueryRowContext(ctx, file.ObjectID, file.VersionNumber, file.SizeBytes, file.ETag, file.StorageClass).Scan(&newVersionID)
+	if err != nil {
+		ov.loggerService.Error(commonErrors.FailedToExecuteInsertQuery, map[string]any{
+			"query": query,
+			"args": map[string]any{
+				"object_id":      file.ObjectID,
+				"version_number": file.VersionNumber,
+				"size_bytes":     file.SizeBytes,
+				"etag":           file.ETag,
+				"storage_class":  file.StorageClass,
+			},
+			"error": err.Error(),
+		})
+		return 0, err
+	}
+
+	return newVersionID, nil
+}
+
+func (ov *VersionRepository) UpdateCurrentVersionIDOfObject(ctx context.Context, tx *sql.Tx, objectID int, versionID int) error {
+	query := `UPDATE objects SET current_version_id = $1 WHERE id = $2`
+	stmt, err := tx.PrepareContext(ctx, query)
+	if err != nil {
+		ov.loggerService.Error(commonErrors.FailedToPrepareQuery, map[string]any{
+			"query": query,
+			"args": map[string]any{
+				"object_id":          objectID,
+				"current_version_id": versionID,
+			},
+			"error": err.Error(),
+		})
+		return err
+	}
+	defer func() {
+		if closeErr := stmt.Close(); closeErr != nil {
+			ov.loggerService.Error(commonErrors.FailedToCloseStatement, closeErr)
+		}
+	}()
+	_, err = stmt.ExecContext(ctx, versionID, objectID)
+	if err != nil {
+		ov.loggerService.Error(commonErrors.FailedToExecuteUpdateQuery, map[string]any{
+			"query": query,
+			"args": map[string]any{
+				"object_id":          objectID,
+				"current_version_id": versionID,
+			},
+			"error": err.Error(),
+		})
+		return err
+	}
+
+	return nil
+}
+
+func (ov *VersionRepository) GetNewVersionNumber(ctx context.Context, tx *sql.Tx, objectID int) (int, error) {
+	query := `SELECT COALESCE(MAX(version_number), 0) + 1 FROM object_versions WHERE object_id = $1  `
+	stmt, err := tx.PrepareContext(ctx, query)
+	if err != nil {
+		ov.loggerService.Error(commonErrors.FailedToPrepareQuery, map[string]any{
+			"query": query,
+			"args": map[string]any{
+				"object_id": objectID,
+			},
+			"error": err.Error(),
+		})
+		return 0, err
+	}
+	defer func() {
+		if closeErr := stmt.Close(); closeErr != nil {
+			ov.loggerService.Error(commonErrors.FailedToCloseStatement, closeErr)
+		}
+	}()
+	var newVersionNumber int
+	err = stmt.QueryRowContext(ctx, objectID).Scan(&newVersionNumber)
+	if err != nil {
+		ov.loggerService.Error(commonErrors.FailedToExecuteInsertQuery, map[string]any{
+			"query": query,
+			"args": map[string]any{
+				"object_id": objectID,
+			},
+			"error": err.Error(),
+		})
+		return 0, err
+	}
+
+	return newVersionNumber, nil
+}
