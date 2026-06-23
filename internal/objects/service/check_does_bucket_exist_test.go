b9c50b7e929c4f7c3dd05e29e6a3ec2111f7d5f0
Adrian Kurek <adikurek11@gmail.com>
2026-06-23 19:59:01 +0200

    test: created tests for checkPermissions, checkDoesBucketExists and one test for create methods of object service

---
+++ internal/objects/service/check_does_bucket_exist_test.go
@@ -0,0 +1,90 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	bucketMocks "github.com/slodkiadrianek/MINI-BUCKET/test/mocks/bucket"
+	objectMocks "github.com/slodkiadrianek/MINI-BUCKET/test/mocks/objects"
+	permissionMocks "github.com/slodkiadrianek/MINI-BUCKET/test/mocks/permissions"
+	versionMocks "github.com/slodkiadrianek/MINI-BUCKET/test/mocks/versions"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCheckDoesBucketExist(t *testing.T) {
+	type args struct {
+		title     string
+		setupMock func() (permissionRepository, objectRepository, versionRepository, bucketRepository)
+		wantErr   bool
+		err       error
+	}
+
+	testScenarios := []args{
+		{
+			title: "with proper data",
+			setupMock: func() (permissionRepository, objectRepository, versionRepository, bucketRepository) {
+				mPermissionRepository := new(permissionMocks.MockPermissionRepository)
+				mObjectRepository := new(objectMocks.MockObjectRepository)
+				mVersionRepository := new(versionMocks.MockVersionRepository)
+				mBucketRepository := new(bucketMocks.MockBucketRepository)
+				mBucketRepository.On("Exists", mock.Anything, mock.Anything).Return(true, nil)
+				return mPermissionRepository, mObjectRepository, mVersionRepository, mBucketRepository
+			},
+			wantErr: false,
+			err:     nil,
+		},
+		{
+			title: "bucket does not exists",
+			setupMock: func() (permissionRepository, objectRepository, versionRepository, bucketRepository) {
+				mPermissionRepository := new(permissionMocks.MockPermissionRepository)
+				mObjectRepository := new(objectMocks.MockObjectRepository)
+				mVersionRepository := new(versionMocks.MockVersionRepository)
+				mBucketRepository := new(bucketMocks.MockBucketRepository)
+				mBucketRepository.On("Exists", mock.Anything, mock.Anything).Return(false, nil)
+				return mPermissionRepository, mObjectRepository, mVersionRepository, mBucketRepository
+			},
+			wantErr: true,
+			err:     errors.New("api error: bucket with provided id does not exist"),
+		},
+		{
+			title: "failed db query Exists failed ",
+			setupMock: func() (permissionRepository, objectRepository, versionRepository, bucketRepository) {
+				mPermissionRepository := new(permissionMocks.MockPermissionRepository)
+				mObjectRepository := new(objectMocks.MockObjectRepository)
+				mVersionRepository := new(versionMocks.MockVersionRepository)
+				mBucketRepository := new(bucketMocks.MockBucketRepository)
+				mBucketRepository.On("Exists", mock.Anything, mock.Anything).Return(false, errors.New("failed to perform query"))
+				return mPermissionRepository, mObjectRepository, mVersionRepository, mBucketRepository
+			},
+			wantErr: true,
+			err:     errors.New("failed to perform query"),
+		},
+	}
+
+	for _, testScenario := range testScenarios {
+		t.Run(testScenario.title, func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+			defer cancel()
+
+			db, _, _ := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherRegexp))
+
+			permissionRepository, objectRepository, versionRepository, bucketRepository := testScenario.setupMock()
+			loggerService := setupObjectServiceDependencies()
+			objectService := NewObjectService(loggerService, objectRepository, permissionRepository, bucketRepository, db, versionRepository)
+
+			err := objectService.checkDoesBucketExist(ctx, 1)
+			if (err != nil) != testScenario.wantErr {
+				t.Errorf("CheckDoesBucketExist() error = %v, wantErr = %v", err, testScenario.wantErr)
+			}
+
+			if err != nil && testScenario.err != nil {
+				if err.Error() != testScenario.err.Error() {
+					t.Errorf("CheckDoesBucketExist() error = %v, scenarioError = %v", err, testScenario.err)
+				}
+			}
+		})
+	}
+}
