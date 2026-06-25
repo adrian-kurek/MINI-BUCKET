f985f85861eff0c6478ce4c782b40760ab4a614e
slodkiadrianek <adikurek11@gmail.com>
2026-06-25 18:19:02 +0200

    refactor: adapted models and dto to new databse schema

--- internal/objects/DTO/object.go
+++ internal/objects/DTO/object.go
@@ -5,15 +5,27 @@ import (
 )
 
 type Create struct {
-	BucketID    int
-	ObjectKey   string
-	ContentType string
-	SizeBytes   int
-	ETag        string
+	BucketID     int
+	ObjectKey    string
+	ContentType  string
+	SizeBytes    int
+	ETag         string
+	StorageClass string
+	UUID         string
+}
+
+type Update struct {
+	ObjectID     int
+	SizeBytes    int
+	ETag         string
+	StorageClass string
+	UUID         string
 }
 
 type IncomingFile struct {
-	File        io.Reader
-	ContentType string
-	SizeBytes   int
+	File         io.Reader
+	FileName     string
+	ContentType  string
+	StorageClass string
+	SizeBytes    int
 }
