b6915cc38dea9175c1749fcd541c0e3e5e2a5c0a
adrian-kurek <adikurek11@gmail.com>
2026-07-23 19:31:43 +0200

    tests: created tests for DeleteMany method

--- internal/objects/repository/delete_test.go
+++ internal/objects/repository/delete_test.go
@@ -11,56 +11,53 @@ import (
 	objectRepository "github.com/slodkiadrianek/MINI-BUCKET/internal/objects/repository"
 )
 
-
-
 func TestDeleteOne(t *testing.T) {
 	type args struct {
-		title string
-		setupMock func () (*sql.DB)
-		wantErr bool
-		err error
+		title     string
+		setupMock func() *sql.DB
+		wantErr   bool
+		err       error
 	}
 
 	testScenarios := []args{
 		{
 			title: "with proper data",
 			setupMock: func() *sql.DB {
-				db,mock,_ := sqlmock.New()
+				db, mock, _ := sqlmock.New()
 				mock.ExpectPrepare(regexp.QuoteMeta("DELETE FROM objects WHERE object_key = $1")).
-				ExpectExec().
-				WithArgs(sqlmock.AnyArg()).
-				WillReturnResult(sqlmock.NewResult(0,1))
+					ExpectExec().
+					WithArgs(sqlmock.AnyArg()).
+					WillReturnResult(sqlmock.NewResult(0, 1))
 				return db
 			},
 			wantErr: false,
-			err: nil,
+			err:     nil,
 		},
 		{
 			title: "failed to prepare query",
 			setupMock: func() *sql.DB {
-				db,mock,_ := sqlmock.New()
+				db, mock, _ := sqlmock.New()
 				mock.ExpectPrepare(regexp.QuoteMeta("DELETE FROM objects WHERE object_key = $1")).
-				WillReturnError(errors.New("failed to prepare query"))
+					WillReturnError(errors.New("failed to prepare query"))
 				return db
 			},
 			wantErr: true,
-			err: errors.New("failed to prepare query"),
+			err:     errors.New("failed to prepare query"),
 		},
 		{
 			title: "failed to execute query",
 			setupMock: func() *sql.DB {
-				db,mock,_ := sqlmock.New()
+				db, mock, _ := sqlmock.New()
 				mock.ExpectPrepare(regexp.QuoteMeta("DELETE FROM objects WHERE object_key = $1")).
-				ExpectExec().
-				WithArgs(sqlmock.AnyArg()).
-				WillReturnError(errors.New("failed to execute query"))
+					ExpectExec().
+					WithArgs(sqlmock.AnyArg()).
+					WillReturnError(errors.New("failed to execute query"))
 				return db
 			},
 			wantErr: true,
-			err: errors.New("failed to execute query"),
+			err:     errors.New("failed to execute query"),
 		},
 	}
-
 
 	for _, testscenario := range testScenarios {
 		t.Run(testscenario.title, func(t *testing.T) {
@@ -68,8 +65,8 @@ func TestDeleteOne(t *testing.T) {
 			db := testscenario.setupMock()
 
 			loggerservice := setupObjectRepositoryDependencies()
-			repo := objectRepository.New( db,loggerservice)
-			err := repo.DeleteOne(ctx,"test")
+			repo := objectRepository.New(db, loggerservice)
+			err := repo.DeleteOne(ctx, "test")
 
 			if (err != nil) != testscenario.wantErr {
 				t.Errorf("DeleteOne() error = %v, wantErr = %v", err, testscenario.wantErr)
@@ -78,6 +75,76 @@ func TestDeleteOne(t *testing.T) {
 			if err != nil && testscenario.err != nil {
 				if err.Error() != testscenario.err.Error() {
 					t.Errorf("DeleteOne() error = %v, scenarioError = %v", err, testscenario.err)
+				}
+			}
+		})
+	}
+}
+
+func TestDeleteMany(t *testing.T) {
+	type args struct {
+		title     string
+		setupMock func() *sql.DB
+		wantErr   bool
+		err       error
+	}
+
+	testScenarios := []args{
+		{
+			title: "with proper data",
+			setupMock: func() *sql.DB {
+				db, mock, _ := sqlmock.New()
+				mock.ExpectPrepare(regexp.QuoteMeta("DELETE FROM objects WHERE object_key IN ( $1 )")).
+					ExpectExec().
+					WithArgs(sqlmock.AnyArg()).
+					WillReturnResult(sqlmock.NewResult(0, 1))
+				return db
+			},
+			wantErr: false,
+			err:     nil,
+		},
+		{
+			title: "failed to prepare query",
+			setupMock: func() *sql.DB {
+				db, mock, _ := sqlmock.New()
+				mock.ExpectPrepare(regexp.QuoteMeta("DELETE FROM objects WHERE object_key IN ( $1 )")).
+					WillReturnError(errors.New("failed to prepare query"))
+				return db
+			},
+			wantErr: true,
+			err:     errors.New("failed to prepare query"),
+		},
+		{
+			title: "failed to execute query",
+			setupMock: func() *sql.DB {
+				db, mock, _ := sqlmock.New()
+				mock.ExpectPrepare(regexp.QuoteMeta("DELETE FROM objects WHERE object_key IN ( $1 )")).
+					ExpectExec().
+					WithArgs(sqlmock.AnyArg()).
+					WillReturnError(errors.New("failed to execute query"))
+				return db
+			},
+			wantErr: true,
+			err:     errors.New("failed to execute query"),
+		},
+	}
+
+	for _, testscenario := range testScenarios {
+		t.Run(testscenario.title, func(t *testing.T) {
+			ctx := context.Background()
+			db := testscenario.setupMock()
+
+			loggerservice := setupObjectRepositoryDependencies()
+			repo := objectRepository.New(db, loggerservice)
+			err := repo.DeleteMany(ctx, []string{"test"})
+
+			if (err != nil) != testscenario.wantErr {
+				t.Errorf("DeleteMany() error = %v, wantErr = %v", err, testscenario.wantErr)
+			}
+
+			if err != nil && testscenario.err != nil {
+				if err.Error() != testscenario.err.Error() {
+					t.Errorf("DeleteMany() error = %v, scenarioError = %v", err, testscenario.err)
 				}
 			}
 		})
