96017006cd76d3f1c25900a7046cbe01dc3e84c3
adrian-kurek <adikurek11@gmail.com>
2026-07-29 20:43:28 +0200

    tests: written tests for  UpdateCurrentVersionIDsOfObjects method

--- internal/objects/repository/update_test.go
+++ internal/objects/repository/update_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"log"
 	"regexp"
 	"testing"
@@ -13,20 +14,19 @@ import (
 	objectRepository "github.com/slodkiadrianek/MINI-BUCKET/internal/objects/repository"
 )
 
-
 func TestUpdate(t *testing.T) {
 	type args struct {
-		title string
-		setupMock func () (*sql.DB)
-		wantErr bool
-		err error
+		title     string
+		setupMock func() *sql.DB
+		wantErr   bool
+		err       error
 	}
 
 	testScenarios := []args{
 		{
 			title: "with proper data",
-			setupMock: func ()(*sql.DB) {
-				db,mock,_ := sqlmock.New()
+			setupMock: func() *sql.DB {
+				db, mock, _ := sqlmock.New()
 				mock.ExpectBegin()
 				mock.ExpectPrepare(regexp.QuoteMeta(`UPDATE objects SET 
 					size_bytes = $1,
@@ -40,12 +40,12 @@ func TestUpdate(t *testing.T) {
 				return db
 			},
 			wantErr: false,
-			err: nil,
+			err:     nil,
 		},
 		{
 			title: "failed to prepare query",
-			setupMock: func ()(*sql.DB) {
-				db,mock,_ := sqlmock.New()
+			setupMock: func() *sql.DB {
+				db, mock, _ := sqlmock.New()
 				mock.ExpectBegin()
 				mock.ExpectPrepare(regexp.QuoteMeta(`UPDATE objects SET 
 					size_bytes = $1,
@@ -57,12 +57,12 @@ func TestUpdate(t *testing.T) {
 				return db
 			},
 			wantErr: true,
-			err: errors.New("failed to prepare query"),
+			err:     errors.New("failed to prepare query"),
 		},
 		{
 			title: "failed to execute query",
-			setupMock: func ()(*sql.DB) {
-				db,mock,_ := sqlmock.New()
+			setupMock: func() *sql.DB {
+				db, mock, _ := sqlmock.New()
 				mock.ExpectBegin()
 				mock.ExpectPrepare(regexp.QuoteMeta(`UPDATE objects SET 
 					size_bytes = $1,
@@ -77,9 +77,8 @@ func TestUpdate(t *testing.T) {
 				return db
 			},
 			wantErr: true,
-			err: errors.New("failed to execute query"),
+			err:     errors.New("failed to execute query"),
 		},
-
 	}
 
 	for _, testscenario := range testScenarios {
@@ -97,15 +96,15 @@ func TestUpdate(t *testing.T) {
 			}()
 
 			loggerservice := setupObjectRepositoryDependencies()
-			repo := objectRepository.New( db,loggerservice)
+			repo := objectRepository.New(db, loggerservice)
 			bucketInput := DTO.Update{
-				SizeBytes: 1,
-				ETag: "test",
+				SizeBytes:    1,
+				ETag:         "test",
 				StorageClass: "STANDARD",
-				UUID: "1234",
-				ObjectID: 1,
+				UUID:         "1234",
+				ObjectID:     1,
 			}
-			err = repo.Update(ctx,tx,  bucketInput)
+			err = repo.Update(ctx, tx, bucketInput)
 
 			if (err != nil) != testscenario.wantErr {
 				t.Errorf("Update() error = %v, wantErr = %v", err, testscenario.wantErr)
@@ -114,6 +113,98 @@ func TestUpdate(t *testing.T) {
 			if err != nil && testscenario.err != nil {
 				if err.Error() != testscenario.err.Error() {
 					t.Errorf("Update() error = %v, scenarioError = %v", err, testscenario.err)
+				}
+			}
+		})
+	}
+}
+
+func TestUpdateCurrentVersionIDsOfObjects(t *testing.T) {
+	type args struct {
+		title     string
+		setupMock func() (*sql.DB, context.Context)
+		wantErr   bool
+		err       error
+	}
+
+	testScenarios := []args{
+		{
+			title: "with proper data",
+			setupMock: func() (*sql.DB, context.Context) {
+				db, mock, _ := sqlmock.New()
+				ctx := context.Background()
+				placeholder := "($1,$2)"
+				expectedQuery := fmt.Sprintf("UPDATE objects o SET current_version_id = d.current_version_id from (values %s ) as d(object_id, current_version_id) WHERE o.id = d.object_id", placeholder)
+				mock.ExpectBegin()
+				mock.ExpectPrepare(regexp.QuoteMeta(expectedQuery)).
+					ExpectExec().
+					WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg()).
+					WillReturnResult(sqlmock.NewResult(1, 1))
+				mock.ExpectCommit()
+				return db, ctx
+			},
+			wantErr: false,
+			err:     nil,
+		},
+		{
+			title: "failed to prepare sql query",
+			setupMock: func() (*sql.DB, context.Context) {
+				db, mock, _ := sqlmock.New()
+				ctx := context.Background()
+				placeholder := "($1,$2)"
+				expectedQuery := fmt.Sprintf("UPDATE objects o SET current_version_id = d.current_version_id from (values %s ) as d(object_id, current_version_id) WHERE o.id = d.object_id", placeholder)
+				mock.ExpectBegin()
+				mock.ExpectPrepare(regexp.QuoteMeta(expectedQuery)).
+					WillReturnError(errors.New("failed to prepare sql query"))
+				mock.ExpectRollback()
+				return db, ctx
+			},
+			wantErr: true,
+			err:     errors.New("failed to prepare sql query"),
+		},
+		{
+			title: "failed to execute sql query",
+			setupMock: func() (*sql.DB, context.Context) {
+				db, mock, _ := sqlmock.New()
+				ctx := context.Background()
+				placeholder := "($1,$2)"
+				expectedQuery := fmt.Sprintf("UPDATE objects o SET current_version_id = d.current_version_id from (values %s ) as d(object_id, current_version_id) WHERE o.id = d.object_id", placeholder)
+				mock.ExpectBegin()
+				mock.ExpectPrepare(regexp.QuoteMeta(expectedQuery)).
+					ExpectExec().
+					WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg()).
+					WillReturnError(errors.New("failed to execute sql query"))
+				mock.ExpectRollback()
+				return db, ctx
+			},
+			wantErr: true,
+			err:     errors.New("failed to execute sql query"),
+		},
+	}
+
+	for _, testScenario := range testScenarios {
+		t.Run(testScenario.title, func(t *testing.T) {
+			loggerService := setupObjectRepositoryDependencies()
+			db, ctx := testScenario.setupMock()
+			repo := objectRepository.New(db, loggerService)
+			tx, err := db.BeginTx(context.Background(), nil)
+			if err != nil {
+				panic(err)
+			}
+			defer func() {
+				if closeErr := tx.Rollback(); closeErr != nil {
+					log.Println("failed to roll back query", closeErr)
+				}
+			}()
+
+			err = repo.UpdateCurrentVersionIDsOfObjects(ctx, tx, []int{1}, []int{1})
+			if (err != nil) != testScenario.wantErr {
+				t.Errorf("UpdateCurrentVersionIDsOfObjects() error = %v, wantErr = %v", err, testScenario.wantErr)
+			}
+
+			if err != nil && testScenario.err != nil {
+				if err.Error() != testScenario.err.Error() {
+					t.Errorf("UpdateCurrentVersionIDsOfObjects() error = %v, scenarioError = %v", err, testScenario.err)
 				}
 			}
 		})
